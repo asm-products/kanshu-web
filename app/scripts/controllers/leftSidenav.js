cb0876a6102ea26160f6a2ed3ccf59d5c13d1dbc
Claire Tang <myfavecolorisgreen@gmail.com>
2015-02-18 17:55:28 -0800

    updated sidenav to reflect new rows, including drill down to change selected topics via checkboxes

--- app/scripts/controllers/leftSidenav.js
+++ app/scripts/controllers/leftSidenav.js
@@ -11,23 +11,54 @@ angular.module('kanshuWebApp')
   .controller('LeftSidenavCtrl', ['$scope', function ($scope) {
     $scope.rows = [
     	[{"src": "images/ic_poll_48px.svg",
-    	"text": "My Progress"},
-    	{"src": "images/ic_poll_black_48dp.png",
-    	"text": "My Saved Characters"},
-    	{"src": "images/ic_assignment_black_48dp.png",
-    	"text": "Practice Exercises"}],
+    	"text": "Topics",
+        "type": "toggle",
+        "onClick": function(){
+            $scope.showingTopics = !$scope.showingTopics;
+        }}],
     	[{"src": "images/ic_extension_black_48dp.png",
-    	"text": "Add Ons"},
-    	{"src": "images/ic_person_black_48dp.png",
-    	"text": "My Account"},
-    	{"src": "images/ic_settings_black_48dp.png",
-    	"text": "Settings"}],
+    	"text": "Add Ons"}],
+    	[{"src": "images/ic_settings_black_48dp.png",
+    	"text": "Upgrade"}],
     	[{"src": "images/ic_help_black_48dp.png",
-    	"text": "Help"},
-    	{"src": "images/kanshu_about_gray.png",
+    	"text": "Help"}],
+    	[{"src": "images/kanshu_about_gray.png",
     	"text": "About"}],
-    	[{"src": "images/ic_search_black_48dp.png",
-    	"text": "Sign Out"}]
-
     ];
+
+    $scope.showingTopics = false;
+
+    $scope.topics = [
+        {"topic": "China", 
+        "checked": true},
+        {"topic": "Travel", 
+        "checked": true},
+        {"topic": "News and Politics", 
+        "checked": true},
+        {"topic": "Sports", 
+        "checked": true},
+        {"topic": "Games", 
+        "checked": true},
+        {"topic": "Humor", 
+        "checked": true},
+        {"topic": "Science and Technology",
+        "checked": true},
+        {"topic": "Finance and Economics", 
+        "checked": true},
+        {"topic": "Fashion and Lifestyle", 
+        "checked": true},
+        {"topic": "Literature", 
+        "checked": true},
+        {"topic": "Entertainment", 
+        "checked": true},
+        {"topic": "History and Culture",
+        "checked": true},
+        {"topic": "Spirituality", 
+        "checked": true},
+        {"topic": "Art", 
+        "checked": true},
+        {"topic": "Auto",
+        "checked": true},
+    ];
+
   }]);
