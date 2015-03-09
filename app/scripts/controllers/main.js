e7c5ad05f4e90598fcf00a1501259a2fcb3ffb89
Alexandre Grison <a.grison@gmail.com>
2015-03-09 13:48:56 +0100

    Bounty 144: Skeleton of the Log In Dialog
    Working on bounty 144 (see https://assembly.com/kanshu/bounties/144).
    The colored panda icon (kanshu_about_colors.png) needs some love.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,42 @@
  * Controller of the kanshuWebApp
  */
 angular.module('kanshuWebApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
+  .controller('MainCtrl', ['$scope', 'UserService', '$mdDialog',
+                  function ($scope, UserService, $mdDialog) {
   	$scope.shouldBeLockedOpen = false;
+
+    $scope.isLoggedIn = false;
 
   	$scope.toggleSidenav = function() {
   		$scope.shouldBeLockedOpen = !$scope.shouldBeLockedOpen;
   	};
 
+    $scope.login = function(user) {
+      $scope.isLoggedIn = UserService.login(user.email, user.password);
+    };
+
+    $scope.logout = function() {
+      UserService.logout();
+      $scope.isLoggedIn = false;
+    };
+
+    $scope.showLoginDialog = function(ev) {
+            $mdDialog.show({
+                templateUrl: 'views/login-dialog.html',
+                targetEvent: ev,
+                controller: function($scope, $mdDialog) {
+                              $scope.submit = function() {
+                                  $mdDialog.hide($scope.user);
+                              };
+
+                              $scope.cancel = function() {
+                                  $mdDialog.cancel();
+                              };
+                          }
+            })
+            .then(function(user) {
+                $scope.login(user);
+            });
+        };
 
   }]);
