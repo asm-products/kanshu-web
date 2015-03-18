dbb00d21b4d0135224eca9a85c898615c165319b
Claire Tang <myfavecolorisgreen@gmail.com>
2015-03-18 13:58:10 -0700

    #132, dialog cancel redirects to articles

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,9 +1,13 @@
 'use strict';
 
-angular.module('kanshuWebApp').controller('SignupCtrl', function SignupCtrl($scope, $mdDialog) {
+angular.module('kanshuWebApp').controller('SignupCtrl', function SignupCtrl($scope, $mdDialog, $location) {
   $mdDialog.show({
     parent: angular.element(document.body),
     templateUrl: 'views/signupDialog.html',
     controller: 'SignupDialogCtrl'
+  }).then(function(){
+
+  }, function(){
+  	$location.path('/articles');
   });
 });
